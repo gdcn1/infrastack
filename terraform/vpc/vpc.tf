5ec51c0cf93b4e015df9cb1dc6c2c779fcadf643
Andrey Fogel <afo@tradeshift.com>
2020-03-25 15:09:25 +0700

    Create VPC
    We need to create VPC for goodcoin project

---
+++ terraform/vpc/vpc.tf
@@ -0,0 +1,26 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "goodcoin_vpc"
+  cidr = "172.31.248.0/21"
+
+  azs            = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  public_subnets = ["172.31.252.0/24", "172.31.253.0/24", "172.31.254.0/23"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Name    = "goodcoin_vpc"
+    Purpose = "VPC for goodcoin project"
+    Warning = "Managed by terraform do not edit"
+  }
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
