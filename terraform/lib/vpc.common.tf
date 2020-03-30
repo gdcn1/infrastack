c90297c8b5c90421cbbe4b42685dfe318f3e500d
Andrey Fogel <afo@tradeshift.com>
2020-03-30 21:13:12 +0700

    Fix remote state
    small fix remote state after migration

--- terraform/lib/vpc.common.tf
+++ terraform/lib/vpc.common.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
 
   config = {
-    bucket  = "terraform-states-goodcoin"
+    bucket  = "terraform-states-good-coin"
     key     = "vpc/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
